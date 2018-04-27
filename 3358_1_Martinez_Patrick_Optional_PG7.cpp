82b23914d6c3523604ae8970ccecca9792234d5e
martip23 <patrickmartinez77@gmail.com>
2018-04-27 12:48:17 -0500

    Added file and header comments

---
+++ 3358_1_Martinez_Patrick_Optional_PG7.cpp
@@ -0,0 +1,11 @@
+//      Roster Number: 24
+//
+//      Author: Patrick Martinez
+//      Due Date:  Apr. 30, 2018
+//      Programming Assignment Number 7 - Optional
+//
+//      Spring 2018 - CS 3358 - 1
+//
+//      Instructor: Husain Gholoom.
+//
+//
