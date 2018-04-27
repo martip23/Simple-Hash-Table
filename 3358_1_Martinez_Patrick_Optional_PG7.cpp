dda950d71f193b72fff6d86a797ff1f9ac8b581d
martip23 <patrickmartinez77@gmail.com>
2018-04-27 16:57:47 -0500

    Added professor's code

--- 3358_1_Martinez_Patrick_Optional_PG7.cpp
+++ 3358_1_Martinez_Patrick_Optional_PG7.cpp
@@ -8,6 +8,107 @@
 //
 //      Instructor: Husain Gholoom.
 //
-//
+//      This program does the following, creates a hash table. Populates hash
+//      table halfway with integers from 125 to 425. Displays the array. Allows
+//      searching for a value, allows insertion of new numbers, allows deletion
+//      of numbers.
 
-class
+#include <iostream>
+using namespace std;
+
+class hash{
+private:
+    int hash_pos;
+    int array[40];
+public:
+    hash();
+    void insert();
+    void search();
+    int Hash(int);
+    int reHash(int);
+    void Delete();
+    void Display();
+};
+
+int main () {
+    return 0;
+}
+
+hash::hash() {
+    for (int i = 0; i < 40; i++){
+        array[i] = '*';
+    }
+}
+
+void hash::insert() {
+    int data
+    int count = 0;
+    cout << "Enter the data to insert: ";
+    cin >> data;
+    hash_pos = Hash(data);
+    if (hash_pos >= 40){
+        has_pos = 0;
+    }
+    while(array[hash_pos] != '*'){
+        hash_pos = reHash(hash_pos);
+        count++;
+        if(count >= 40){
+            cout << "Memory Full!! No space is available for storage";
+            break;
+        }
+    }
+    if(array[hash_pos] == '*'){
+        array[hash_pos] = data;
+    }
+    cout << "Data is stored at index " << hash_pos << endl;
+}
+
+int hash::Hash(int key){
+    return key%100;
+}
+
+int hash::reHash(int key){
+    return (key+1)%100;
+}
+
+void hash::search(){
+    int key,i;
+    bool isFound = false;
+    cout << "Enter the key to search: ";
+    cin >> key;
+    for(int i = 0; i < 40; i++) {
+        if(array[i] == key) {
+            isFound = true;
+            break;
+        }
+    }
+    if(isFound){
+        cout << "The key is found at index " << i << endl;
+    } else {
+        cout << "No record found!!" << endl;
+    }
+}
+
+void hash::Delete(){
+    int key, i;
+    bool isFound = false;
+    cout << "Enter the key to delete: ";
+    cin >> key;
+    for (i = 0; i < 40; i++){
+        if (array[i] == key){
+            isFound = true;
+            break;
+        }
+    }
+    if(isFound){
+        array[i] = '*';
+        cout << "The key is deleted" << endl;
+    } else {
+        cout << "No key is found!!";
+    }
+}
+
+void hash::Display(){
+    for (int i = 0; i < 40; i++)
+        cout << array[i] << "  ";
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Simple-Hash-Table
 A simple hash table made to meet requirements of a homework assignment.
+
+As part of the assignment, we were required to build on top of the instructors code. Please contact me for questions on which parts of code are my own. (Hint: Variables and code not following standard convention are not my own :haha: )
